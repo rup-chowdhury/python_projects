81dabb053dda980396f98a5f3cec6badfc5207bd
Rup Chowdhury <rupbd1@gmail.com>
2025-09-15 14:13:45 +0200

    Update word_guessing_game.py

--- word_guessing_game/word_guessing_game.py
+++ word_guessing_game/word_guessing_game.py
@@ -8,21 +8,21 @@ guessedWord = ['_'] * len(word)
 
 attempts = 10
 
-if attempts > 0:
+while attempts > 0:
     print('Current Word: ' + ' '.join(guessedWord))
 
-guess = input('Guess a letter : ').lower()
+    guess = input('Guess a letter : ').lower()
 
-if guess in word:
-    for i in range(len(word)):
-        if word[i] == guess:
-            guessedWord[i] = guess
-    print("Great Guess")
-else:
-    attempts = attempts - 1
-    print("Wrong Guess! Attepts left: " + str(attempts))
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedWord[i] = guess
+        print("Great Guess")
+    else:
+        attempts = attempts - 1
+        print("Wrong Guess! Attepts left: " + str(attempts))
 
-    if '_' not in guessedWord:
+        if '_' not in guessedWord:
             print('\n Congratulations! You guessed the correct word' + word)
             break
 
